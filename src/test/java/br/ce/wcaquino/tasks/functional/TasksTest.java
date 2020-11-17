f477f76d62a087c7b86828247614e952ba455e3e
Your Name <you@example.com>
2020-11-17 08:50:55 -0300

    change ip test server

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -16,8 +16,8 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 //		WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.6:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.6:9999/tasks");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.11:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.1.11:9999/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
