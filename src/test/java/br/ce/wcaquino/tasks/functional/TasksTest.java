de6378a46882c473c8a5ba73c2ab9a65c1843e74
Your Name <you@example.com>
2020-11-18 18:07:33 -0300

    .

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -17,8 +17,8 @@ public class TasksTest {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 //		WebDriver driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), cap);
 //		driver.navigate().to("http://tomcat:8080/tasks/");
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.102:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.102:8001/tasks");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.6:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.1.6:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
